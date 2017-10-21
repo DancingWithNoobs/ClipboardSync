1fdbca1a3b6ac680fa5f087d4195713319b7da93
DancingWithNoobs <DancingWithNoobs@gmail.com>
2017-10-21 13:34:10 +0200

    TEst

--- app/src/main/java/com/cedric/clipboardsync/activities/MainActivity.java
+++ app/src/main/java/com/cedric/clipboardsync/activities/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity
         SetupClipboardChecker();
     }
 
+    
     private void CheckServer()
     {
         TextView notConnectedText = (TextView) this.findViewById(R.id.noServerText);
